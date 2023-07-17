552951a4d8ff3291abe9787b6a4ec593f6c6359f
Nichole Mattera <me@nicholemattera.gay>
2023-07-17 18:18:58 -0500

    🐛 Fixed an issue with trailing slashes

--- cmd/main.go
+++ cmd/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	mux := http.NewServeMux()
+	mux := http.DefaultServeMux
 	api.SetupRouter(mux)
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
--- internal/api/router.go
+++ internal/api/router.go
@@ -10,14 +10,14 @@ func SetupRouter(mux *http.ServeMux) {
 	mux.Handle("/", http.RedirectHandler("https://linktr.ee/OutClimbMN", http.StatusTemporaryRedirect))
 
 	// Events
-	mux.Handle("/events/bouldering-meetup-minneapolis-bouldering-project", EventHandler())
-	mux.Handle("/events/outdoor-climbing", LatestEventHandler())
-	mux.Handle("/events/20230506-outdoor-climbing-taylor-falls", EventHandler())
-	mux.Handle("/events/20230527-outdoor-climbing-taylor-falls", http.RedirectHandler("https://outclimb.gay/events/20230527-outdoor-climbing-saint-croix-falls", http.StatusPermanentRedirect))
-	mux.Handle("/events/20230527-outdoor-climbing-saint-croix-falls", EventHandler())
-	mux.Handle("/events/20230716-outdoor-climbing-sugar-loaf-bluff", EventHandler())
-	mux.Handle("/events/20230730-outdoor-climbing-he-mni-can-barn-bluff", EventHandler())
+	mux.Handle("/events/bouldering-meetup-minneapolis-bouldering-project/", EventHandler())
+	mux.Handle("/events/outdoor-climbing/", LatestEventHandler())
+	mux.Handle("/events/20230506-outdoor-climbing-taylor-falls/", EventHandler())
+	mux.Handle("/events/20230527-outdoor-climbing-taylor-falls/", http.RedirectHandler("https://outclimb.gay/events/20230527-outdoor-climbing-saint-croix-falls", http.StatusPermanentRedirect))
+	mux.Handle("/events/20230527-outdoor-climbing-saint-croix-falls/", EventHandler())
+	mux.Handle("/events/20230716-outdoor-climbing-sugar-loaf-bluff/", EventHandler())
+	mux.Handle("/events/20230730-outdoor-climbing-he-mni-can-barn-bluff/", EventHandler())
 
-	mux.Handle("/update", http.RedirectHandler("https://outclimb.gay/events/20230527-outdoor-climbing-saint-croix-falls", http.StatusTemporaryRedirect))
-	mux.Handle("/contact", ContactHandler())
+	mux.Handle("/update/", http.RedirectHandler("https://outclimb.gay/events/20230527-outdoor-climbing-saint-croix-falls", http.StatusTemporaryRedirect))
+	mux.Handle("/contact/", ContactHandler())
 }
