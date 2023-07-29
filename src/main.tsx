b9e4514aa2b9dad5fa60e5c4ef8316cc1ab3acfd
Nichole Mattera <me@nicholemattera.gay>
2023-07-29 07:33:53 -0500

    ✨ Added 404 page

--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,42 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
 import Contact from './views/Contact/Contact.tsx';
 import Event from './views/Event/Event.tsx';
 import Home from './views/Home/Home.tsx';
+import NotFound from './views/NotFound/NotFound.tsx';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+    createBrowserRouter,
+    Navigate,
+    RouterProvider,
+} from 'react-router-dom';
+
 import './index.css'
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/events/:eventId",
-    element: <Event />
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
-  },
+    {
+        path: '/',
+        element: <Home />,
+    },
+    {
+        path: '/events/:eventId',
+        element: <Event />
+    },
+    {
+        path: '/contact',
+        element: <Contact />,
+    },
+    {
+        path: '/404',
+        element: <NotFound />,
+    },
+    {
+        path: '*',
+        element: <Navigate to='/404' replace />
+    },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>,
 )
---
+++ src/views/NotFound/NotFound.tsx
@@ -0,0 +1,13 @@
+import Header from 'components/Header/Header';
+
+import './NotFound.scss'
+
+function NotFound() {
+    return (
+        <>
+            <Header />
+        </>
+    )
+}
+
+export default NotFound
