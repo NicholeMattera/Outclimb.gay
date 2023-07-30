5f0360d28a9e08927fb7bbe46ba2f73b75d3bcd9
Nichole Mattera <me@nicholemattera.gay>
2023-07-29 21:01:01 -0500

    ✨ Added Slot component and useSlots hook

---
+++ src/components/Slot/Slot.tsx
@@ -0,0 +1,7 @@
+import SlotProps from 'types/SlotProps';
+
+function Slot({ children }: SlotProps) {
+    return children;
+}
+
+export default Slot
---
+++ src/types/SlotProps.ts
@@ -0,0 +1,8 @@
+import React from 'react';
+
+type SlotProps = {
+    children?: React.ReactNode
+    name: string
+};
+
+export default SlotProps;
