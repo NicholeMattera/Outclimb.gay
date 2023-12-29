56c8013ec5e5b6c470e50682fe2194332083ba53
Nichole Mattera <me@nicholemattera.gay>
2023-12-29 11:13:10 -0600

    🔨 Disable build cache on deploy and lookaside scripts

--- scripts/deploy/index.sh
+++ scripts/deploy/index.sh
@@ -4,22 +4,22 @@ echo "🧹 Cleaning up temp directory"
 rm -rf /tmp/outclimb
 
 echo "📦 Downloading the latest"
-git clone https://github.com/OutClimb/Outclimb.gay.git /tmp/outclimb
+git clone --depth 1 https://github.com/OutClimb/Outclimb.gay.git /tmp/outclimb
 cd /tmp/outclimb
 
 echo "🩹 Patching configs"
 patch -i ~/secrets.diff
 
 echo "🛠️ Building"
-docker compose build
+docker compose build --no-cache
 
 echo "🪦 Bringing down and removing old container"
 docker container stop outclimb-outclimb-1
 docker container rm outclimb-outclimb-1
 
 echo "💡 Bringing up new container"
-docker compose up -d
+docker compose up --detach
 
 echo "🧹 Cleaning up old images and temp directory"
-docker image prune -a -f
+docker image prune --all --force
 rm -rf /tmp/outclimb
--- scripts/lookaside/index.sh
+++ scripts/lookaside/index.sh
@@ -8,14 +8,14 @@ git clone --branch $2 --depth 1 https://github.com/OutClimb/Outclimb.gay.git /tm
 cd /tmp/outclimb-lookaside
 
 if [ $1 = "create" ]; then
-    docker build --build-arg="BRANCH_NAME=$2" --tag outclimbgay_create-lookaside:latest -f scripts/lookaside/create-lookaside.dockerfile .
+    docker build --no-cache --build-arg="BRANCH_NAME=$2" --tag outclimbgay_create-lookaside:latest -f scripts/lookaside/create-lookaside.dockerfile .
     docker run -e BRANCH_NAME=$2 -v outclimbgay3_lookasides:/lookaside outclimbgay_create-lookaside
 
     echo "🧹 Cleaning up container and image"
     docker container rm outclimbgay_create-lookaside
     docker image rm outclimbgay_create-lookaside:latest
 elif [ $1 = "delete" ]; then
-    docker build --tag outclimbgay_delete-lookaside:latest -f scripts/lookaside/delete-lookaside.dockerfile .
+    docker build --no-cache --tag outclimbgay_delete-lookaside:latest -f scripts/lookaside/delete-lookaside.dockerfile .
     docker run -e BRANCH_NAME=$2 -v outclimbgay3_lookasides:/lookaside outclimbgay_delete-lookaside
 
     echo "🧹 Cleaning up container and image"
